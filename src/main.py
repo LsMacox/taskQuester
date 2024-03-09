9aed6424165b738df08bf6cdae95abc980ff6d85
Adam Zugiraev <adam.zugiraev@onlinesup.com>
2024-03-09 21:09:26 +0300

    Return work state

--- src/main.py
+++ src/main.py
@@ -32,22 +32,11 @@ async def main() -> None:
 
 
 if __name__ == "__main__":
-    # asyncio.run(main())
-
-    ssl_context = {}
-    port = 8000
-
-    if not settings.DEBUG:
-        ssl_context = {
-            "ssl_certfile": settings.BASE_DIR / 'contrib' / 'ssl' / 'cert.pem',
-            "ssl_keyfile": settings.BASE_DIR / 'contrib' / 'ssl' / 'key.pem',
-        }
-        port = 443
+    asyncio.run(main())
 
     if "--webserver" in args:
         uvicorn.run(
             app="services.fastapi_app:app",
             host="0.0.0.0",
             port=8000,
-            # **ssl_context,
         )
